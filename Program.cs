28d49dcf7d633d459546592351a05c46c29e533d
adriano <adriano.profile.ads.cloud@gmail.com>
2024-05-23 01:27:42 -0300

    Finalização do Desáfio-Projto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("87910325","Iphone 13","20248791",8);
+iphone.Ligar();
+iphone.InstalarAplicativo("Discord");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("8791000","Nokia 7","20243591", 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("Evernote");
